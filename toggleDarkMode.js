a12019bb776b1925fa81841cf2e5f944391d738d
James Atkin <curlyw42@gmail.com>
2020-04-01 22:55:50 +0100

    Added dark mode to other pages

---
+++ toggleDarkMode.js
@@ -0,0 +1,19 @@
+function toggleLightDark() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+
+    // Change sun and moon icon
+    element = document.getElementById("btnToggle");
+    element.classList.toggle("light");
+    $("#btnToggle i").toggleClass("fa-moon-o fa-sun-o")
+
+    // a elements
+    var a = document.querySelectorAll("a");
+    for (var i = 0; i < a.length; i++) {
+      a[i].classList.toggle("dark-mode");
+    }
+
+    // Footer
+    element = document.getElementById("footer-text");
+    element.classList.toggle("small text-white-50");
+}
